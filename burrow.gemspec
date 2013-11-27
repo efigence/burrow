1e1c0b19e0dbcb8275d89c1f71a00cab584f6b04
Krzysztof Zych <krzysztof.zych@efigence.com>
2013-11-27 15:03:45 +0100

    Gemspec + code from internal version.

--- burrow.gemspec
+++ burrow.gemspec
@@ -8,8 +8,8 @@ Gem::Specification.new do |spec|
   spec.version       = Burrow::VERSION
   spec.authors       = ["Krzysztof Zych"]
   spec.email         = ["krzysztof.zych@efigence.com"]
-  spec.description   = %q{TODO: Write a gem description}
-  spec.summary       = %q{TODO: Write a gem summary}
+  spec.description   = %q{Instead of writing code to estabilish your queues and exchanges, declare them in a simple format and build it with a single method.}
+  spec.summary       = %q{Declaratively express your AMQP topology when connecting.}
   spec.homepage      = ""
   spec.license       = "MIT"
 
@@ -20,4 +20,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 1.3"
   spec.add_development_dependency "rake"
+  spec.add_runtime_dependency "bunny", "~> 1.0"
 end
--- lib/burrow.rb
+++ lib/burrow.rb
@@ -1,3 +1,38 @@
 module Burrow
   VERSION = "0.1.0"
+  @@conn_cache = {}
+
+  class << self
+    def setup config
+      conn = get_connection config['url']
+      channel = conn.create_channel
+      setup_topology channel, config['topology']
+    end
+
+    private
+
+    def setup_topology channel, topology
+      ns = {}
+      topology.map do |defn|
+        obj, name = create_object channel, defn
+        ns[name] = obj
+        if bind_to = defn['bind_to']
+          obj.bind ns[bind_to], symbolize_keys(defn['bind_options'] || {})
+        end
+        obj
+      end.last
+    end
+
+    def create_object channel, defn
+      method = defn['type'].to_sym
+      name = defn['name'] || ''
+      opts = symbolize_keys(defn['options'] || {})
+      obj = channel.send(method, name, opts)
+      return [obj, obj.name]
+    end
+
+    def get_connection url
+      @@conn_cache[url] ||= Bunny.new(url).tap { |conn| conn.start }
+    end
+  end
 end
