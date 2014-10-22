9f40f2760226fe25a723a1c35954290655792404
Krzysztof Zych <krzysztof.zych@efigence.com>
2014-10-22 10:56:47 +0200

    pozwól ustawiać client_info; bump do 0.4.0

--- lib/burrow.rb
+++ lib/burrow.rb
@@ -1,4 +1,6 @@
 require 'burrow/version'
+require 'bunny'
+require 'bunny/session'
 
 module Burrow
   @@conn_cache = {}
@@ -10,16 +12,16 @@ module Burrow
     # @param config [Hash] configuration hash, must include 'url' and 'topology' keys. See README for topology descriptions.
     # @param force [Boolean] (optional) discard a cached connection for this particular url, useful in case of network errors
     # @return [Bunny::Exchange, Bunny::Queue] final object in the topology list
-    def setup config, force=false
+    def setup(config, force=false)
       drop_connection_from_cache(config['url']) if force
-      conn = get_connection config['url']
+      conn = get_connection config['url'], get_client_info(config)
       channel = conn.create_channel
       setup_topology channel, config['topology']
     end
 
     private
 
-    def setup_topology channel, topology
+    def setup_topology(channel, topology)
       ns = {}
       topology.map do |defn|
         obj, name = create_object channel, defn
@@ -38,7 +40,7 @@ module Burrow
       end.last
     end
 
-    def create_object channel, defn
+    def create_object(channel, defn)
       method = defn['type'].to_sym
       name = defn['name'] || ''
       opts = symbolize_keys(defn['options'] || {})
@@ -46,15 +48,25 @@ module Burrow
       return [obj, obj.name]
     end
 
-    def get_connection url
-      @@conn_cache[url] ||= Bunny.new(url).tap { |conn| conn.start }
+    def get_connection(url, client_properties=nil)
+      options = {}
+      options.merge!(properties: client_properties) if client_properties.is_a? Hash
+      @@conn_cache[url] ||= Bunny.new(url, options).tap { |conn| conn.start }
     end
 
-    def drop_connection_from_cache url
+    def drop_connection_from_cache(url)
       @@conn_cache.delete url
     end
 
-    def symbolize_keys hash
+    def get_client_info(config)
+      opts = Bunny::Session::DEFAULT_CLIENT_PROPERTIES.dup
+      if config['client'].is_a? Hash
+        opts.merge! symbolize_keys(config['client'])
+      end
+      opts
+    end
+
+    def symbolize_keys(hash)
       Hash[hash.map do |key, value|
         if key.respond_to?(:to_sym)
           [key.to_sym, value]
